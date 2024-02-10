3e6a52e687ddb5df8ea20a3a431c43d5a2f6de00
wasatchgeek <159587694+wasatchgeek@users.noreply.github.com>
2024-02-10 08:37:59 -0700

    fixing syntax again ..

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
 
-  vpc_security_group_ids = [aws_security_group_blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
