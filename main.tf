25a7d564044c2b6ac04241ec639ded26462de038
wasatchgeek <159587694+wasatchgeek@users.noreply.github.com>
2024-02-10 09:37:03 -0700

    creating new vpc

--- main.tf
+++ main.tf
@@ -20,12 +20,28 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  
+  subnet_id = module.blog_vpc.public_subnets[0]
 
 
   tags = {
@@ -38,7 +54,7 @@ module "blog_sg" {
   version = "4.13.0"
   name = "blog_new"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.blog_vpc.vpc_id
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -50,7 +66,7 @@ module "blog_sg" {
 
 /*
  Manual way to do without using modules from terraform doc
- 
+
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
