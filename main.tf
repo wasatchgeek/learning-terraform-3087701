dcedd0f360f2758592b1116e53d1da477e657507
wasatchgeek <159587694+wasatchgeek@users.noreply.github.com>
2024-02-10 08:46:17 -0700

    doing my syntax errors

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = aws_security_group.blog.id
   }
 
 resource "aws_security_group_rule" "blog_https_in" {
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = aws_security_group.blog.id
   }
 
 
@@ -68,5 +68,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = aws_security_group.blog.id
   }
